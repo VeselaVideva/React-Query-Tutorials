03ad63387cf001d6b9e5974d1f548d66e878c68d
Vesela Videva <vesela.videva@gmail.com>
2022-08-24 13:32:59 +0300

    Custom hook for fetching hero data by ID

---
+++ react-query-starter/src/hooks/useSuperHeroData.js
@@ -0,0 +1,11 @@
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+const fetchSuperhero = ({ queryKey }) => {
+    const heroId = queryKey[1]
+    return axios.get(`http://localhost:4000/superheroes/${heroId}`)
+  }
+
+export const useSuperHeroeData = (heroId) => {
+    return useQuery(['super-hero', heroId], fetchSuperhero)
+}
